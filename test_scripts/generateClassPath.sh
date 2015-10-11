85faf65d66c61ecb176a4ce9a24c92df68b08929
Damien Zufferey <zufferey@csail.mit.edu>
2015-10-11 18:19:53 -0400

    build tools

---
+++ test_scripts/generateClassPath.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+file=test_scripts/deps
+
+echo "#this is the classpath wrt the root directory of the project" > $file
+echo -n "cp=\"" >> $file
+sbt "show dependencyClasspath" | tr -d ' ' | tr ',' '\n' | gawk 'match($0, /Attributed\(([^)]*)\)/, a) {print a[1]}' | tr '\n' ':' >> $file
+echo -n "target/scala-2.11/classes:target/scala-2.11/test-classes/" >> $file
+echo '"' >> $file
+echo  >> $file
+
+echo "#with assembly we need only the following two jars:" >> $file
+echo "#cp=\"target/scala-2.11/psync-assembly-0.1-SNAPSHOT.jar:target/scala-2.11/psync_2.11-0.1-SNAPSHOT-tests.jar\"" >> $file
+
