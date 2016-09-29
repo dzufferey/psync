a2eed36d457cb16305db53fb61de8b8a201f4425
Damien Zufferey <zufferey@csail.mit.edu>
2016-09-29 17:01:02 -0400

    travis z3 installation script

--- .ci/install_z3.sh
+++ .ci/install_z3.sh
@@ -2,7 +2,11 @@
 
 set -x
 
-if [ ! -d "$HOME/z3" ]; then
+if [ ! -f "$HOME/z3/z3-4.4.1-x64-ubuntu-14.04/bin/z3" ]; then
+  mkdir -p $HOME/z3
+  if [ "$(ls -A $HOME/z3)" ]; then
+      rm -r $HOME/z3/*
+  fi
   wget https://github.com/Z3Prover/z3/releases/download/z3-4.4.1/z3-4.4.1-x64-ubuntu-14.04.zip -O ~/z3.zip
   unzip ~/z3.zip -d ~/z3
 fi
