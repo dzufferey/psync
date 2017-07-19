636c03c84be9921b5c735906ddc93e13dcbc06b3
Damien Zufferey <zufferey@mpi-sws.org>
2017-07-19 10:50:55 +0200

    updating z3 version in travis build

--- .ci/install_z3.sh
+++ .ci/install_z3.sh
@@ -2,14 +2,16 @@
 
 set -x
 
-if [ ! -f "$HOME/z3/z3-4.4.1-x64-ubuntu-14.04/bin/z3" ]; then
+version=4.5.0
+
+if [ ! -f "$HOME/z3/z3-${version}-x64-ubuntu-14.04/bin/z3" ]; then
   mkdir -p $HOME/z3
   if [ "$(ls -A $HOME/z3)" ]; then
       rm -r $HOME/z3/*
   fi
-  wget https://github.com/Z3Prover/z3/releases/download/z3-4.4.1/z3-4.4.1-x64-ubuntu-14.04.zip -O ~/z3.zip
+  wget https://github.com/Z3Prover/z3/releases/download/z3-${version}/z3-${version}-x64-ubuntu-14.04.zip -O ~/z3.zip
   unzip ~/z3.zip -d ~/z3
 fi
 
-PATH="$HOME/z3/z3-4.4.1-x64-ubuntu-14.04/bin/:$PATH"
+PATH="$HOME/z3/z3-${version}-x64-ubuntu-14.04/bin/:$PATH"
 z3 --version
