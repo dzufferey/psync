0f288d88d338dc197d0f5a68c6a7240feee04c33
Damien Zufferey <zufferey@mpi-sws.org>
2018-12-19 19:17:56 +0100

    some more operations on Progress

--- src/main/scala/psync/Progress.scala
+++ src/main/scala/psync/Progress.scala
@@ -10,6 +10,10 @@ class Progress(val value: Long) extends AnyVal {
 
   def timeout: Long = Progress.getTimeout(this)
 
+  def orElse(p: Progress) = Progress.orElse(this, p)
+  def lub(p: Progress) = Progress.lub(this, p)
+  def glb(p: Progress) = Progress.glb(this, p)
+
   override def toString = {
     if (isWaitMessage) {
       if (isStrict) "StrictWaitForMessage"
@@ -54,6 +58,42 @@ object Progress {
     assert(isTimeout(p))
     val noMask = (p.value & mask)
     (noMask << 2) >> 2 //to get the sign back
+  }
+
+  @inline final def orElse(p1: Progress, p2: Progress): Progress = if (p1 != unchanged) p1 else p2
+
+  //max timeout and strictness
+  @inline final def lub(p1: Progress, p2: Progress): Progress = {
+    assert(p1 != unchanged && p2 != unchanged)
+    val strict = isStrict(p1) || isStrict(p2)
+    if (isWaitMessage(p1) || isWaitMessage(p2)) {
+      if (strict) strictWaitMessage else waitMessage
+    } else if (p1 == goAhead) {
+      p2
+    } else if (p2 == goAhead) {
+      p1
+    } else {
+      val to1 = getTimeout(p1)
+      val to2 = getTimeout(p2)
+      val to = math.max(to1, to2)
+      if (strict) strictTimeout(to) else timeout(to)
+    }
+  }
+
+  //min timeout and strictness
+  @inline final def glb(p1: Progress, p2: Progress): Progress = {
+    assert(p1 != unchanged && p2 != unchanged)
+    val strict = isStrict(p1) && isStrict(p2)
+    if (p1 == goAhead || p2 == goAhead) {
+      goAhead
+    } else if (isWaitMessage(p1) && isWaitMessage(p2)) {
+      if (strict) strictWaitMessage else waitMessage
+    } else {
+      val to1 = if (isTimeout(p1)) getTimeout(p1) else getTimeout(p2)
+      val to2 = if (isTimeout(p2)) getTimeout(p2) else to1
+      val to = math.min(to1, to2)
+      if (strict) strictTimeout(to) else timeout(to)
+    }
   }
 
   final def timeoutInBounds(l: Long) = l == ((l << 2) >> 2)
