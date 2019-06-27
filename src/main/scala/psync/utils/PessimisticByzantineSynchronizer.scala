29174327de6f5f6e00f0f52c2c6629214bd8ab2f
Damien Zufferey <zufferey@mpi-sws.org>
2019-06-27 14:02:04 +0200

    byzantine sync

--- src/main/scala/psync/utils/PessimisticByzantineSynchronizer.scala
+++ src/main/scala/psync/utils/PessimisticByzantineSynchronizer.scala
@@ -8,12 +8,15 @@ import scala.reflect.ClassTag
 // works with f < n/3
 // this does not make the round Byzantine tolerant, the round does still needs to deal with faulty messages. It only deal with synchronization.
 
-//TODO test
+//TODO may need two timeouts
+// - long timeouts until receive n-f
+// - short timeouts after n-f
+// - go ahead when has n messages
 
 class PessimisticByzantineSynchronizer[A: ClassTag: KryoRegistration](
     round: EventRound[A],       //the round to synchronize
-    defaultTO: Long,            //a default timeout value
-    waitMsg: Boolean = true    //if we can assume reliable channels between correct processes we can always wait for enogh messages and stay synchronized
+    shortTimeout: Long,         //a short timeout value (to get some more messages afte we have #msg to progress)
+    longTimeout: Long           //a long timeout value (to wait for the minimum #msg to progress)
   ) extends EventRound[Option[A]] {
 
   //progress status of the argument round
@@ -22,14 +25,16 @@ class PessimisticByzantineSynchronizer[A: ClassTag: KryoRegistration](
   //counting messages for the byzantine sychronization
   protected var nMsg = 0
   protected var nf = 0
+  protected var n = 0
 
   def init: Progress = {
     rProgress = round.init
     nMsg = 0
-    nf = group.size - group.nbrByzantine
+    n = group.size
+    nf = n - group.nbrByzantine
     val syncProgress =
-      if (waitMsg) Progress.waitMessage
-      else Progress.timeout(defaultTO)
+      if (longTimeout > 0) Progress.timeout(longTimeout)
+      else Progress.waitMessage
     Progress.lub(rProgress, syncProgress)
   }
 
@@ -45,8 +50,13 @@ class PessimisticByzantineSynchronizer[A: ClassTag: KryoRegistration](
       rProgress = round.receive(sender, msg).orElse(rProgress) //if 'unchanged' keep the old one
     }
     val syncProgress =
-      if (nMsg > nf || !waitMsg) Progress.timeout( defaultTO )
-      else Progress.waitMessage
+      if (nMsg > nf) { // enough messages to progress
+        if (nMsg == n || shortTimeout <= 0) Progress.goAhead
+        else Progress.timeout( shortTimeout )
+      } else { // need more messages
+        if (longTimeout > 0) Progress.timeout( longTimeout )
+        else Progress.waitMessage
+      }
     Progress.lub(rProgress, syncProgress)
   }
 
--- src/test/scala/example/byzantine/DummyByzantineTest.scala
+++ src/test/scala/example/byzantine/DummyByzantineTest.scala
@@ -7,7 +7,7 @@ import psync.macros.Macros._
 import psync.utils._
 import psync.utils.serialization._
 
-class DBTProcess(timeout: Long, wait: Boolean) extends Process[Unit] {
+class DBTProcess(short: Long, timeout: Long) extends Process[Unit] {
 
   def init(io: Unit) { }
 
@@ -29,15 +29,15 @@ class DBTProcess(timeout: Long, wait: Boolean) extends Process[Unit] {
         Progress.goAhead
       }
 
-    }, timeout, wait)
+    }, short, timeout)
   )
 }
 
-class DummyByzantineTest(rt: Runtime, timeout: Long, _wait: Boolean) extends Algorithm[Unit,DBTProcess](rt) {
+class DummyByzantineTest(rt: Runtime, short: Long, timeout: Long) extends Algorithm[Unit,DBTProcess](rt) {
   
   val spec = TrivialSpec
 
-  def process = new DBTProcess(timeout, _wait)
+  def process = new DBTProcess(short, timeout)
 
   def dummyIO = ()
 
@@ -48,12 +48,12 @@ object DummyByzantineRunner extends Runner {
   override def defaultConfFile = "src/test/resources/sample-conf.xml"
 
   _nbrByzantine = 1
-  _delayFirstSend = 3000
-  var _wait = true
-  newOption("--noWait", dzufferey.arg.Unit( () => _wait = false), "always TO")
+  //_delayFirstSend = 3000
+  var short = 1l
+  newOption("--short", dzufferey.arg.Int( i => short = i), "short TO")
 
   def onStart {
-    val alg = new DummyByzantineTest(rt, timeout, _wait)
+    val alg = new DummyByzantineTest(rt, short, timeout)
     Thread.sleep(100)
     Console.println("replica " + id + " starting")
     alg.startInstance(0, ())
